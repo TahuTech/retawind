d877219cc9ba5bd8bf6eb981675cc177f115017b
TahuTech <18053036@student.uwp.ac.id>
2022-11-15 11:58:52 +0700

    add layanan website

---
+++ src/views/Website.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navbar from "components/Navbars/AuthNavbar.js";
+import Footer from "components/Footers/Footer.js";
+
+export default function Website() {
+    return (
+        <>
+            <Navbar transparent />
+            <main className="layanan-website">
+
+
+            </main>
+            <footer/>
+        </>
+    );
+}
